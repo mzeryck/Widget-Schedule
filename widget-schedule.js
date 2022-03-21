dce2a7995c3a4410ebc980350e306336ef5196c4
Max Zeryck <71023727+mzeryck@users.noreply.github.com>
2022-03-20 18:01:43 -0700

    better OOP

--- widget-schedule.js
+++ widget-schedule.js
@@ -5,93 +5,23 @@
 // Do not modify the following comment.
 // THIS_IS_WIDGET_SCHEDULE
 
-class RowItem extends UITableRow {
-  constructor(name) {
-    super()
-    this.name = name
-    this.height = 55
-    this.dismissOnSelect = false
-    this.cellSpacing = 0
-  
-    this.onSelect = async () => {
-      const newName = await promptForScript(this)
-      if (this.defaultRow) schedule.defaultWidget = newName
-      else await updateSchedule(newName, this.start, this.end, true)
-      await loadTable()
-    }
-  }
-}
-
-class DefaultRow extends RowItem {
-  constructor(name) {
-    super(name)
-    this.defaultRow = true
-    const cell = this.addText(this.name, this.description())
-    cell.subtitleColor = Color.gray()
-    cell.widthWeight = 70
-  }
-  
-  description() {
-    return "Default"
-  }
-}
-
-class TimedRow extends RowItem {
-  constructor(name, start, end) {
-    super(name)
-    this.start = start
-    this.end = end
-    this.defaultRow = false
-    
-    const cell = this.addText(this.name, this.description())
-    cell.subtitleColor = Color.gray()
-    cell.widthWeight = 70
-    
-    const startButton = this.addButton("Start")
-    const endButton = this.addButton("End")
-    const delButton = this.addButton("\u274c")
-    startButton.widthWeight = 15
-    endButton.widthWeight = 15
-    delButton.widthWeight = 10
-  
-    startButton.onTap = async () => {
-      await changeStart(this, await pickTime(Schedule.indexToDate(this.start),null,Schedule.indexToDate(this.end)))
-      await loadTable()
-    }
-  
-    endButton.onTap = async () => {
-      await changeEnd(this, await pickTime(Schedule.indexToDate(this.end + 1), Schedule.indexToDate(this.start + 1), Schedule.indexToDate(96)) - 1) // broken lol
-      await loadTable()
-    }
-  
-    delButton.onTap = async () => {
-      if (!(await shouldPerformDestructive(`Are you sure you want to delete the ${this.description()} schedule for ${this.name}?`, "Delete"))) return
-      await updateSchedule("", this.start, this.end, true)
-      await loadTable()
-    }
-  }
-  
-  description() {
-    return Schedule.indexToString(this.start) + "-" + Schedule.indexToString(this.end+1)
-  }
-}
-
 class Schedule {
-
-  constructor(defaultWidget, times) {
+  constructor(name, defaultWidget, times) {
+    this.name = name
     this.defaultWidget = defaultWidget
     this.times = times || new Array(48).fill("")
   }
   
-  async currentWidget(fm) {
-    const directoryPath = fm.joinPath(fm.documentsDirectory(), "Widget Schedule")
-    if (!fm.fileExists(directoryPath) || !fm.isDirectory(directoryPath)) fm.createDirectory(directoryPath)
+  async currentWidget() {
+    const folderPath = Schedule.folderPath()
+    if (!fm.fileExists(folderPath) || !fm.isDirectory(folderPath)) fm.createDirectory(folderPath)
     
     const name = this.times[Schedule.dateToIndex(new Date())] || this.defaultWidget
     const scriptPath = fm.joinPath(fm.documentsDirectory(), name + ".js")
-    const exportPath = fm.joinPath(directoryPath, name)
+    const exportPath = fm.joinPath(folderPath, name + ".widget")
     
-    if (!fm.fileExists(exportPath) || fm.modificationDate(scriptPath) > fm.modificationDate(exportPath)) {  
+    if (!fm.fileExists(exportPath) || fm.modificationDate(scriptPath) > fm.modificationDate(exportPath)) {
+      await iCloudCheck(scriptPath)
       const scriptString = "module.exports = async function() {" + fm.readString(scriptPath) + "}"
       fm.writeString(exportPath, scriptString.replace(/Script.name\(\)/g,`"${name}"`))
     }
@@ -100,14 +30,40 @@ class Schedule {
     await exportFunction()
   }
   
-  static fromFile(fm, path) {
-    if (!fm.fileExists(path)) return null
-    const file = JSON.parse(fm.readString(path))
-    return new Schedule(file.defaultWidget, file.times)
+  save() {
+    fm.writeString(Schedule.path(this.name), JSON.stringify(this))
   }
   
-  static toFile(schedule, fm, path) {
-    fm.writeString(path, JSON.stringify(schedule))
+  updateDefault(newDefault) {
+    this.defaultWidget = newDefault
+    this.save()
+  }
+  
+  async update(name, start, end, suppressAlert) {
+    if (!suppressAlert && this.times.slice(start, end).some(r => r.length && r != name)) {
+      if (!(await showDestructive("This will overwrite other parts of the schedule. Do you want to continue?", "Continue"))) return false
+    }
+    for (let i=start; i <= end; i++) {
+      this.times[i] = name
+    }
+    this.save()
+    return true
+  }
+  
+  static path(name) {
+    return fm.joinPath(Schedule.folderPath(), name + ".schedule")
+  }
+  
+  static folderPath() {
+    return fm.joinPath(fm.documentsDirectory(), "Widget Schedule")
+  }
+  
+  static async load(name) {
+    const path = Schedule.path(name)
+    if (!fm.fileExists(path)) return null
+    await iCloudCheck(path)
+    const file = JSON.parse(fm.readString(path))
+    return new Schedule(name, file.defaultWidget, file.times)
   }
   
   static indexToString(index) {
@@ -127,131 +83,262 @@ class Schedule {
   static dateToIndex(date) {
     return (date.getHours() * 2) + (Math.floor(date.getMinutes() / 30))
   }
-
 }
 
-let fm, path, widget, schedule, table
+class ScheduleTable extends UITable {
+  constructor(schedule) {
+    super()
+    this.schedule = schedule
+    this.showSeparators = true
+  }
+  
+  async update() {
+    this.removeAllRows()
+  
+    const headerRow = new UITableRow()
+    headerRow.isHeader = true
+    headerRow.height = 75
+  
+    const headline = headerRow.addText(this.schedule.name,"Widget Schedule")
+    headline.widthWeight = 70
+  
+    const settingsButton = headerRow.addButton("Settings")
+    settingsButton.widthWeight = 30
+    settingsButton.rightAligned()
+  
+    settingsButton.onTap = async () => {
+      const response = await showAlert("Settings", ["Update code","Close settings"])
+      if (response == 0) {
+        await updateCode()
+      }
+    }
+  
+    this.addRow(headerRow)
+    this.addRow(new DefaultRow(this.schedule.defaultWidget, this))
 
+    let i, name, start
+    for (i = 0; i <= this.schedule.times.length; i++) {
+      const current = this.schedule.times[i]
+      if (name == current) continue
+      if (name) this.addRow(new TimedRow(name, this, start, i-1))
+  
+      name = current.length ? current : null
+      start = current.length ? i : null
+    }
+
+    const addRow = new UITableRow()
+    addRow.height = 55
+    addRow.dismissOnSelect = false
+  
+    const addCell = addRow.addText("Add to Schedule")
+    addCell.titleColor = Color.blue()
+    addRow.onSelect = async () => {
+      this.addItem()
+    }
+    this.addRow(addRow)
+    
+    this.reload()
+  }
+  
+  async changeScript(item) {
+    const newName = await pickScript()
+    if (!newName) return
+    if (item.defaultRow) this.schedule.updateDefault(newName)
+    else await this.schedule.update(newName, item.start, item.end, true)
+    this.update()
+  }
+  
+  async changeStart(item) {
+    let newStart = await pickTime(Schedule.indexToDate(item.start),null,Schedule.indexToDate(item.end))
+    if (!newStart) return
+    
+    newStart = Schedule.dateToIndex(newStart)
+    if (item.start == newStart) return
+    
+    if (newStart < item.start) await this.schedule.update(item.name, newStart, item.end)
+    else await this.schedule.update("", item.start, newStart - 1, true)
+    this.update()
+  }
+
+  async changeEnd(item) {
+    let newEnd = await pickTime(Schedule.indexToDate(item.end + 1), Schedule.indexToDate(item.start + 1), Schedule.indexToDate(96))
+    if (!newEnd) return
+    
+    newEnd = Schedule.dateToIndex(newEnd) - 1
+    if (item.end == newEnd) return
+    
+    if (newEnd > item.end) await this.schedule.update(item.name, item.start, newEnd)
+    else await this.schedule.update("", newEnd + 1, item.end, true)
+    this.update()
+  }
+  
+  async removeItem(item) {
+    if (!(await showDestructive(`Are you sure you want to delete the ${item.description()} schedule for ${item.name}?`, "Delete"))) return
+    await this.schedule.update("", item.start, item.end, true)
+    this.update()
+  }
+
+  async addItem() {
+    const name = await pickScript()
+    if (!name) return await showAlert("No script was entered.")
+    
+    let start = await pickTime()
+    if (!start) return await showAlert("No start time was entered.")
+    start = Schedule.dateToIndex(start)
+    
+    const minEnd = Schedule.indexToDate(start+1)
+    let end = await pickTime(minEnd, minEnd)
+    if (!end) return await showAlert("No end time was entered.")
+    end = Schedule.dateToIndex(end) - 1
+    
+    await this.schedule.update(name, start, end)
+    this.update()
+  }
+}
+
+class RowItem extends UITableRow {
+  constructor(name, table) {
+    super()
+    this.name = name
+    this.table = table
+    
+    this.height = 55
+    this.dismissOnSelect = false
+    this.cellSpacing = 0
+  
+    this.onSelect = async () => {
+      this.table.changeScript(this)
+    }
+  }
+  
+  static textCell(item) {
+    const cell = item.addText(item.name, item.description())
+    cell.subtitleColor = Color.gray()
+    cell.widthWeight = 70
+    return cell
+  }
+}
+
+class DefaultRow extends RowItem {
+  constructor(name, table) {
+    super(name, table)
+    this.defaultRow = true
+    RowItem.textCell(this)
+  }
+  
+  description() {
+    return "Default"
+  }
+}
+
+class TimedRow extends RowItem {
+  constructor(name, table, start, end) {
+    super(name, table)
+    this.defaultRow = false
+    this.start = start
+    this.end = end
+    RowItem.textCell(this)
+    
+    const startButton = this.addButton("Start")
+    const endButton = this.addButton("End")
+    const delButton = this.addButton("\u274c")
+    startButton.widthWeight = 15
+    endButton.widthWeight = 15
+    delButton.widthWeight = 10
+  
+    startButton.onTap = async () => {
+      this.table.changeStart(this)
+    }
+  
+    endButton.onTap = async () => {
+      this.table.changeEnd(this)
+    }
+  
+    delButton.onTap = async () => {
+      this.table.removeItem(this)
+    }
+  }
+  
+  description() {
+    return Schedule.indexToString(this.start) + "-" + Schedule.indexToString(this.end+1)
+  }
+}
+
+/* 
+ * Launch
+ * -------------------------------------------- */
+
+let fm
 await launch()
 
 async function launch() {
   fm = FileManager.local()
   fm = (fm.isFileStoredIniCloud(module.filename)) ? FileManager.iCloud() : fm
-  path = fm.joinPath(fm.libraryDirectory(), Script.name() + "-schedule")
+  const schedule = await Schedule.load(Script.name())
   
-  if (!fm.fileExists(path)) {
+  if (!schedule) {
     if (config.runsInApp) return await setup()
-    widget = new ListWidget()
+    const widget = new ListWidget()
     widget.addText("This widget is not set up. Open the Scriptable app and run this script to set up your schedule.")
     return Script.setWidget(widget)
   }
   
-  schedule = Schedule.fromFile(fm, path)
-  if (config.runsInApp) return await edit()
-  return await schedule.currentWidget(fm)
+  if (config.runsInApp) return await edit(schedule)
+  return await schedule.currentWidget()
 }
 
 async function setup() {
-  if (await generateAlert("Welcome to Widget Schedule! Make sure your widget has the name you want before you begin.",['I like the name "' + Script.name() + '"', "Let me go change it"])) return
+  if (await showAlert("Welcome to Widget Schedule! Make sure your widget has the name you want before you begin.",['I like the name "' + Script.name() + '"', "Let me go change it"])) return
   
-  await generateAlert("On the next screen, choose the widget that will display by default when no other widgets are scheduled.")
-  const defaultWidget = await promptForScript()
-  if (!defaultWidget) return await generateAlert("You need to enter the name of a widget.")
+  await showAlert("On the next screen, choose the widget that will display by default when no other widgets are scheduled.")
+  const defaultWidget = await pickScript()
+  if (!defaultWidget) return await showAlert("You need to select a default widget.")
   
-  schedule = new Schedule(defaultWidget)
-  return await edit()
+  const folderPath = Schedule.folderPath()
+  if (!fm.fileExists(folderPath) || !fm.isDirectory(folderPath)) fm.createDirectory(folderPath)
+  
+  const schedule = new Schedule(Script.name(), defaultWidget)
+  schedule.save()
+  return await edit(schedule)
 }
 
-async function edit() {
-  table = new UITable()
-  table.showSeparators = true
-  await loadTable()
+async function edit(schedule) {
+  const table = new ScheduleTable(schedule)
+  await table.update()
   await table.present()
 }
-  
-async function loadTable() {
-  Schedule.toFile(schedule, fm, path)
-  table.removeAllRows()
-  
-  const headerRow = new UITableRow()
-  headerRow.isHeader = true
-  headerRow.height = 75
-  
-  const headline = headerRow.addText(Script.name(),"Widget Schedule")
-  headline.widthWeight = 70
-  
-  const settingsButton = headerRow.addButton("Settings")
-  settingsButton.widthWeight = 30
-  settingsButton.rightAligned()
-  
-  settingsButton.onTap = async () => {
-    const response = await generateAlert("Settings", ["Update code","Close settings"])
-    if (response == 0) {
-      
-    }
-  }
-  
-  table.addRow(headerRow)
 
-  table.addRow(new DefaultRow(schedule.defaultWidget))
+/* 
+ * Helpers
+ * -------------------------------------------- */
 
-  let i, name, start
-  for (i = 0; i <= schedule.times.length; i++) {
-    const current = schedule.times[i]
-    if (name == current) continue
-    if (name) table.addRow(new TimedRow(name, start, i-1))
-  
-    name = current.length ? current : null
-    start = current.length ? i : null
-  }
-
-  const addRow = new UITableRow()
-  addRow.height = 55
-  addRow.dismissOnSelect = false
-  
-  const addCell = addRow.addText("Add to Schedule")
-  addCell.titleColor = Color.blue()
-  addRow.onSelect = async () => {
-    await addItem()
-    await loadTable()
-  }
-  
-  table.addRow(addRow)
-  table.reload()
+async function showAlert(message, buttons=["OK"]) {
+  const alert = new Alert()
+  alert.message = message
+  for (button of buttons) { alert.addAction(button) }
+  return await alert.present()
 }
 
-async function updateSchedule(name, start, end, suppressAlert) {
-  if (!suppressAlert && schedule.times.slice(start, end).some(r => r.length && r != name)) {
-    if (!(await shouldPerformDestructive("This will overwrite other parts of the schedule. Do you want to continue?", "Continue"))) return
-  }
-  for (i=start; i <= end; i++) {
-    schedule.times[i] = name
-  }
+async function showDestructive(message, destructive) {
+  const alert = new Alert()
+  alert.message = message
+  alert.addAction("Cancel")
+  alert.addDestructiveAction(destructive)
+  return await alert.present()
 }
 
-async function changeStart(item, newStart) {
-  if (item.start == newStart) return
-  if (newStart > item.start) return await updateSchedule("", item.start, newStart - 1)
-  await updateSchedule(item.name, newStart, item.end)
+async function pickTime(initial, min, max) { 
+  const picker = new DatePicker()
+  picker.minuteInterval = 30
+
+  if (initial) picker.initialDate = initial
+  if (min) picker.minimumDate = min
+  if (max) picker.maximumDate = max
+  
+  return await picker.pickTime()
 }
 
-async function changeEnd(item, newEnd) {
-  if (item.end == newEnd) return
-  if (newEnd < item.end) return await updateSchedule("", newEnd + 1, item.end)
-  await updateSchedule(item.name, item.start, newEnd)
-}
-
-async function addItem() {
-  const name = await promptForScript()
-  if (!name || !name.length) return await generateAlert("No script was entered.")
-  const start = await pickTime()
-  const minEnd = Schedule.indexToDate(start+1)
-  const end = await pickTime(minEnd, minEnd)
-  await updateSchedule(name, start, end-1)
-}
-
-async function promptForScript() {
-
+async function pickScript() {
   const scripts = new UITable()
   scripts.showSeparators = true
   
@@ -264,7 +351,7 @@ async function promptForScript() {
     if (fm.fileExtension(filename) != 'js') continue
     
     const filePath = fm.joinPath(fm.documentsDirectory(), filename)
-    if (fm.isFileStoredIniCloud(filePath)) await fm.downloadFileFromiCloud(filePath)
+    await iCloudCheck(filePath)
     const file = fm.readString(filePath)
     
     if (file.includes("// THIS_IS_WIDGET_SCHEDULE")) continue
@@ -287,45 +374,9 @@ async function promptForScript() {
     
     scripts.addRow(row)
   }
-  await scripts.present()
-  return promise
-}
-
-async function promptForText(title, message, placeholder, text) {
-  const alert = new Alert()
-  alert.title = title
-  alert.message = message
-  alert.addTextField(placeholder,text)
-  alert.addAction("OK")
-  await alert.present()
-  return alert.textFieldValue(0)
-}
-
-async function generateAlert(message, buttons=["OK"]) {
-  const alert = new Alert()
-  alert.message = message
-  for (button of buttons) { alert.addAction(button) }
-  return await alert.present()
-}
-
-async function shouldPerformDestructive(message, destructive) {
-  const alert = new Alert()
-  alert.message = message
-  alert.addAction("Cancel")
-  alert.addDestructiveAction(destructive)
-  return await alert.present()
-}
-
-async function pickTime(initial, min, max) { 
-  const picker = new DatePicker()
-  picker.minuteInterval = 30
-
-  if (initial) picker.initialDate = initial
-  if (min) picker.minimumDate = min
-  if (max) picker.maximumDate = max
   
-  const time = await picker.pickTime()
-  return Schedule.dateToIndex(time)
+  resolve((await scripts.present()).length)
+  return promise
 }
 
 async function updateCode() {
@@ -337,5 +388,9 @@ async function updateCode() {
       message = "The code has been updated. If the script is open, close it for the change to take effect."
     }
   } catch { }
-  await generateAlert(message)
+  await showAlert(message)
+}
+
+async function iCloudCheck(path) {
+  if (fm.isFileStoredIniCloud(path)) await fm.downloadFileFromiCloud(path)
 }
